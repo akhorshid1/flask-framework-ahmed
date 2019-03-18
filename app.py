a8e15e0f58baf4b6041839455bf8f1d54887f447
Ahmed Khorshid <akhorshid@gmail.com>
2019-03-18 15:10:44 -0400

    added bokeh plot

--- app.py
+++ app.py
@@ -2,13 +2,13 @@ from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-  return render_template('index.html')
+#@app.route('/')
+#def index():
+#  return render_template('index.html')
 
-@app.route('/about')
-def about():
-  return render_template('about.html')
+#@app.route('/about')
+#def about():
+#  return render_template('about.html')
 
 @app.route('/classifications')
 def classifications():
