aabd97160721412567d5cb59faded54c4714535a
Ahmed Khorshid <akhorshid@gmail.com>
2019-03-18 15:25:30 -0400

    added bokeh plot

--- app.py
+++ app.py
@@ -4,15 +4,15 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-  return render_template('index.html')
+  return render_template('classifications.html')
 
 @app.route('/about')
 def about():
   return render_template('about.html')
 
-@app.route('/classifications')
-def classifications():
-  return render_template('classifications.html')
+#@app.route('/classifications')
+#def classifications():
+#  return render_template('classifications.html')
 
 if __name__ == '__main__':
   app.run(port=33507)
